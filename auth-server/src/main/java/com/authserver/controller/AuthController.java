acec5688ecf20d27fc360e01ab800c8f556c11cd
‘LeeJeongHun1’ <wjdgns1155@naver.com>
2024-02-03 22:44:21 +0900

    dockerFile 추가

---
+++ auth-server/src/main/java/com/authserver/controller/AuthController.java
@@ -0,0 +1,17 @@
+package com.authserver.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class AuthController {
+
+
+    @GetMapping
+    public ResponseEntity<String> home() {
+        return ResponseEntity.ok("hello world");
+    }
+}

f045912ca0973594fe5e4db3b3ef5ec71ffbb35b
‘LeeJeongHun1’ <wjdgns1155@naver.com>
2024-02-03 16:28:22 +0900

    멀티 모듈 환경 구성

--- common/src/main/java/com/common/CommonApplication.java
+++ common/src/main/java/com/common/CommonApplication.java
@@ -1,13 +1,13 @@
-package com.example.springbootmsa;
+package com.common;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootMsaApplication {
+public class CommonApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootMsaApplication.class, args);
+        SpringApplication.run(CommonApplication.class, args);
     }
 
 }
--- common/src/test/java/com/common/CommonApplicationTests.java
+++ common/src/test/java/com/common/CommonApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.springbootmsa;
+package com.common;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringBootMsaApplicationTests {
+class CommonApplicationTests {
 
     @Test
     void contextLoads() {
